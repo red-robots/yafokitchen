55131b4d7edfabd831f081f5745f4f8bd2fa9bdf
accrane <austincrane@Austins-MBP-2.lan>
2020-11-30 11:03:39 -0500

    new section

--- page-order.php
+++ page-order.php
@@ -7,7 +7,9 @@ get_header(); ?>
 
 <div id="primary" class="full-content-area clear page-with-banner">
 	<main id="main" class="site-main clear" role="main">
-		<?php while ( have_posts() ) : the_post(); ?>
+		<?php while ( have_posts() ) : the_post();
+			$doorDash = get_field('door_dash');
+		?>
 			<div class="wrapper clear content-center text-center">
 				<header class="entry-header" style="display:none;">
 					<h1 class="entry-title"><?php the_title(); ?></h1>
@@ -32,22 +34,22 @@ get_header(); ?>
 					<div class="flexrow clear">
 					<?php while ( $locations->have_posts() ) : $locations->the_post(); ?>
 						<?php
-						$location_name = get_field('location_name');  
-						$order_link = get_field('order_link');  
-						$address = get_field('address');  
-						$phone = get_field('phone');  
+						$location_name = get_field('location_name');
+						$order_link = get_field('order_link');
+						$address = get_field('address');
+						$phone = get_field('phone');
 						if ($address) { ?>
 						<div class="location fbox text-center">
 							<div class="inside clear">
 								<h3 class="name"><?php echo $location_name ?></h3>
 								<?php if ($order_link) { ?>
-								<div class="buttondiv"><a class="order-button" href="<?php echo $order_link ?>" target="_blank"><span class="icon"><i class="fas fa-utensils"></i></span><span class="txt">Order Now</span></a></div>	
+								<div class="buttondiv"><a class="order-button" href="<?php echo $order_link ?>" target="_blank"><span class="icon"><i class="fas fa-utensils"></i></span><span class="txt">Order Now</span></a></div>
 								<?php } ?>
 								<?php if ($phone) { ?>
-								<address><?php echo $address ?></address>	
+								<address><?php echo $address ?></address>
 								<?php } ?>
 								<?php if ($phone) { ?>
-								<div class="phone"><a href="tel:<?php echo format_phone_number($phone); ?>"><?php echo $phone ?></a></div>	
+								<div class="phone"><a href="tel:<?php echo format_phone_number($phone); ?>"><?php echo $phone ?></a></div>
 								<?php } ?>
 							</div>
 						</div>
@@ -57,7 +59,14 @@ get_header(); ?>
 				</div>
 			</section>
 		<?php } ?>
-
+			<div class="wrapper clear content-center text-center">
+				<header class="entry-header" style="display:none;">
+					<h1 class="entry-title"><?php the_title(); ?></h1>
+				</header>
+				<div class="entry-content">
+					<?php echo $doorDash; ?>
+				</div>
+			</div>
 	</main><!-- #main -->
 </div><!-- #primary -->
 
